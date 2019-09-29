07b5489197c2f051ed42f53d080a3b44a94d864e
Franklin Gabriel Guerra Salazar <gfranklings@gmail.com>
2019-09-29 19:02:11 -0400

    Se customizo la ventana y creado index.html

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
 
 var mainWindow;
 
@@ -6,13 +8,44 @@ console.log("Mesage: Application Launching...");
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
-        title: 'My electron app'
+        title: 'My electron app',
+        icon: path.join(__dirname, 'public/img/favicon/favicon.ico')
     });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    const mainMenu = Menu.buildFromTemplate(applicationMenu);
+    Menu.setApplicationMenu(mainMenu);
 
     mainWindow.on('closed', () => {
         app.quit();
         console.log("Mesage: Application Finished");
     });
 
+    mainWindow.maximize();
+
     console.log("Mesage: Application Started");
 });
+
+function appQuit(){
+    app.quit();
+}
+
+const applicationMenu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Close App',
+                accelerator: process.platform == 'darwin' ? 'command+q' : 'ctrl+q',
+                click(){
+                    appQuit();
+                }
+            }
+        ]
+    }
+]
